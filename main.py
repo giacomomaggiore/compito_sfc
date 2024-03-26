16ffa1638b39cff41ecb54ac2a3f927e50555ca8
giacomo.maggiore <giaco.maggiore@gmail.com>
2024-03-26 18:41:24 +0100

    ppfb

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ yf.pdr_override()
 #-------------------
 start = dt.datetime(2014,10,1)
 end = dt.datetime(2023,12,31)
-stock = "EURUSD=X"
+stock = "PPFB.SG"
 period = 12
 
 df = pdr.get_data_yahoo(stock, start, end)
